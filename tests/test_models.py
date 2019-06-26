fb437e9dd36cbe0167bfeddfdb6819ab02b2bd44
felipe <tifonzafel@gmail.com>
2019-06-26 16:26:51 +0200

    added naive test for training process

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from swan.models import Modeler
 from swan.models.input_validation import validate_input
+from scipy.stats import linregress
 
 path_input = Path("tests/test_files/input_test_models.yml")
 
@@ -24,3 +25,19 @@ def test_modeler():
     xs = map(lambda x: getattr(researcher, x), ('metric', 'opts', 'available_models'))
 
     assert all((x is not None for x in xs))
+
+
+def test_train():
+    """
+    Check the training process
+    """
+    opts = validate_input(path_input)
+    researcher = Modeler(opts)
+
+    model = researcher.train_model()
+
+    rs = model.predict(researcher.data.test)
+
+    data = researcher.data.test.y.reshape(rs.size)
+    print("R2: ", linregress(rs, data))
+
