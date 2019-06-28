d23fa9432b14d35ee24fd3da28a0b4c193d78b0f
felipe <tifonzafel@gmail.com>
2019-06-28 13:54:57 +0200

    added mock test for unifac

--- tests/test_cosmo.py
+++ tests/test_cosmo.py
@@ -1,12 +1,14 @@
-from swan.cosmo.cosmo import compute_activity_coefficient
+from swan.cosmo.cosmo import (call_unifac, compute_activity_coefficient)
 from swan.utils import Options
 from pathlib import Path
+import numpy as np
 import pandas as pd
 import os
 
 
-def test_cosmo_on_chunks(mocker):
+def test_activity_coefficients(mocker):
 
+    output = "Gammas_0.csv"
     try:
         # empty dataframe
         df = pd.DataFrame(columns=["E_solv", "gammas"])
@@ -23,6 +25,26 @@ def test_cosmo_on_chunks(mocker):
         opts = Options(d)
 
         compute_activity_coefficient(opts)
+        assert os.path.exists(output)
 
     finally:
-        os.remove("Gammas_0.csv")
+        os.remove(output)
+
+
+def test_unifac(mocker):
+    """
+    Test the call to unifac
+    """
+    unifac_output = "tests/test_files/unifac_output.out"
+    with open(unifac_output, 'br') as f:
+        xs = f.read()
+
+    # Mock the call to Unifac
+    mocker.patch.dict(os.environ, {'ADFBIN': "tests/test_files/unifac"})
+    mocker.patch("swan.cosmo.cosmo.run_command", return_value=(xs, ()))
+
+    opts = {"solvent": "CC1=CC=CC=C1"}
+
+    x = call_unifac(opts, "CO")
+
+    assert np.allclose(x, 13.6296)
