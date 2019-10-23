66e2895bcbbbfd1535ed357fb2d698af017ac7cb
felipe <tifonzafel@gmail.com>
2019-10-23 09:09:51 +0200

    added nanocat to dependencies

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
     test_suite='tests',
     install_requires=[
         'CAT@git+https://github.com/nlesc-nano/CAT@master',
+        'nano-CAT@git+https://github.com/nlesc-nano/nano-CAT@master',
         'data-CAT@git+https://github.com/nlesc-nano/data-CAT@master',
         'numpy', 'pandas', 'pyyaml>=5.1.1', 'seaborn', 'schema'],
     setup_requires=[
