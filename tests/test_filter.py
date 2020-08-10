e1bfb4c7ce9c1ae560a30cd00fd47f56c7b334c8
felipez <tifonzafel@gmail.com>
2020-08-10 17:16:35 +0200

    refactor test_filter #26

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from .utils_test import PATH_TEST
 from swan.filter.screen import main, apply_filters, read_molecules
 from swan.utils import Options
-from typing import Mapping, TypeVar
+from typing import List, Mapping, TypeVar
 import argparse
 import pandas as pd
 import os
@@ -39,6 +39,18 @@ def remove_output(output_file: str) -> None:
         os.remove(path)
 
 
+def check_expected(opts: Options, expected: List[str]) -> None:
+    """Run a filter workflow using `opts` and check the results."""
+    try:
+        computed = run_workflow(opts)
+        print(f"candidates:\n{computed}")
+        assert all(mol in computed.smiles.values for mol in expected)
+        assert len(computed.smiles) == len(expected)
+
+    finally:
+        remove_output(opts.output_file)
+
+
 def test_filter_cli(mocker) -> None:
     """Test that the CLI works correctly."""
     mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(
@@ -53,18 +65,10 @@ def test_filter_functional_groups(tmp_path) -> None:
     smiles_file = "smiles_functional_groups.csv"
     filters = {"functional_groups": ["C(=O)O"]}
     opts = create_options(filters, smiles_file, tmp_path)
-    try:
-        filter_mols = run_workflow(opts)
-        expected = ("O=C(O)C1CNC2C3CC4C2N4C13", "C#CC12CC(CO1)NCC2C(=O)O",
-                    "CCCCCCCCC=CCCCCCCCC(=O)O", "CC(=O)O",
-                    "O=C(O)Cc1ccccc1", "CC(O)C(=O)O")
-
-        print(f"candidates:\n{filter_mols}")
-        assert all(mol in filter_mols.smiles.values for mol in expected)
-        assert len(filter_mols.smiles) == 6
-
-    finally:
-        remove_output(opts.output_file)
+    expected = ("O=C(O)C1CNC2C3CC4C2N4C13", "C#CC12CC(CO1)NCC2C(=O)O",
+                "CCCCCCCCC=CCCCCCCCC(=O)O", "CC(=O)O",
+                "O=C(O)Cc1ccccc1", "CC(O)C(=O)O")
+    check_expected(opts, expected)
 
 
 def test_filter_bulkiness(tmp_path) -> None:
@@ -74,7 +78,6 @@ def test_filter_bulkiness(tmp_path) -> None:
     opts = create_options(filters, smiles_file, tmp_path)
     opts.core = PATH_TEST / "Cd68Se55.xyz"
     opts.anchor = "O(C=O)[H]"
-    try:
-        filter_mols = run_workflow(opts)
-    finally:
-        remove_output(opts.output_file)
+
+    expected = ("CCCCCCCCC=CCCCCCCCC(=O)O", "CC(=O)O", "CC(O)C(=O)O")
+    check_expected(opts, expected)
