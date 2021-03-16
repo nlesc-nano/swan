2c738ea45b97446171c10a0391babb611b983a87
felipez <tifonzafel@gmail.com>
2021-03-16 09:12:35 +0100

    exploring the equivariant models

--- swan/models/equivariant_models.py
+++ swan/models/equivariant_models.py
@@ -1,14 +1,14 @@
 import torch
 import torch_geometric as tg
 from e3nn import o3
-from e3nn.o3 import FullTensorProduct, FullyConnectedTensorProduct
+from e3nn.o3 import FullyConnectedTensorProduct, TensorProduct
 from flamingo.features.featurizer import (NUMBER_ATOMIC_GRAPH_FEATURES,
                                           NUMBER_BOND_GRAPH_FEATURES)
 from torch_scatter import scatter_add, segment_add_coo
 
 
 class InvariantPolynomial(torch.nn.Module):
-    def __init__(self, irreps_out: str = "0e", lmax: int = 2) -> None:
+    def __init__(self, irreps_out: str = "0e", lmax: int = 3) -> None:
         super().__init__()
 
         # Different bond features
@@ -21,11 +21,18 @@ class InvariantPolynomial(torch.nn.Module):
         representation_atoms = f"{NUMBER_ATOMIC_GRAPH_FEATURES}x0e"
 
         # Tensor product the edge attributes with the harmonic basis
-        self.mul_edges = FullTensorProduct(representation_bonds, self.irreps_sh)
+        # self.mul_edges = FullTensorProduct(representation_bonds, self.irreps_sh)
+        self.mul_edges = TensorProduct(
+            representation_bonds,
+            self.irreps_sh,
+            [(1, ir) for _, ir in self.irreps_sh],
+            [(0, l, l, "uvu", False) for l in range(lmax + 1)]
+        )
 
         # middle layer
-        # irreps_mid = o3.Irreps("64x0e + 24x1e + 24x1o + 16x2e + 16x2o")
-        irreps_mid = o3.Irreps("32x0e + 12x1e + 12x1o + 8x2e + 8x2o")
+        # irreps_mid1 = o3.Irreps("64x0e + 24x1e + 24x1o + 16x2e + 16x2o")
+        irreps_mid1 = o3.Irreps("64x0e + 24x1e + 24x1o + 16x2e + 16x2o")
+        irreps_mid2 = o3.Irreps("8x0e + 4x1e + 4x1o + 2x2e + 2x2o")
 
         # Output representation
         irreps_out = o3.Irreps(irreps_out)
@@ -33,13 +40,20 @@ class InvariantPolynomial(torch.nn.Module):
         self.tp1 = FullyConnectedTensorProduct(
             irreps_in1=representation_atoms,
             irreps_in2=self.mul_edges.irreps_out,
-            irreps_out=irreps_mid,
+            irreps_out=irreps_mid1,
         )
+
         self.tp2 = FullyConnectedTensorProduct(
-            irreps_in1=irreps_mid,
+            irreps_in1=irreps_mid1,
             irreps_in2=self.mul_edges.irreps_out,
-            irreps_out=irreps_out
+            irreps_out=irreps_out,
         )
+
+        # self.tp3 = FullyConnectedTensorProduct(
+        #     irreps_in1=irreps_mid2,
+        #     irreps_in2=self.mul_edges.irreps_out,
+        #     irreps_out=irreps_out
+        # )
 
     def forward(self, data: tg.data.Dataset) -> torch.Tensor:
         # Vector defining the edges
@@ -53,24 +67,29 @@ class InvariantPolynomial(torch.nn.Module):
         # Edge attributes in the harmonic basis
         edge_attr = self.mul_edges(data.edge_attr, edge_sh)
 
-        # Count the number of neighbors for each node
-        num_of_neighbors = tg.utils.degree(edge_dst)
-        non_zero_indices = torch.nonzero(num_of_neighbors).flatten()
-        num_of_bonds_per_atom = num_of_neighbors[non_zero_indices].unsqueeze(-1)
+        # Count the number of neighbors for each atom
+        num_of_bonds_per_atom = tg.utils.degree(edge_dst).unsqueeze(-1)
 
         # For each edge, tensor product the node attributes with the
         # edge features in the spherical harmonics
         edge_features = self.tp1(data.x[edge_src], edge_attr)
         node_features = scatter_add(edge_features, edge_dst, dim=0)
 
-        # Normalize by the number of bonds for each node
+        # Normalize by the number of bonds for each atom
         node_features = node_features.div(num_of_bonds_per_atom ** 0.5)
 
         # communicate information to the neighbors
         edge_features = self.tp2(node_features[edge_src], edge_attr)
         node_features = scatter_add(edge_features, edge_dst, dim=0)
 
-        # Normalize by the number of bonds for each node
+        # # Normalize by the number of bonds for each atom
+        # node_features = node_features.div(num_of_bonds_per_atom ** 0.5)
+
+        # # communicate information to the neighbors
+        # edge_features = self.tp3(node_features[edge_src], edge_attr)
+        # node_features = scatter_add(edge_features, edge_dst, dim=0)
+
+        # Normalize by the number of bonds for each atom
         node_features = node_features.div(num_of_bonds_per_atom ** 0.5)
 
         # Sum for each molecule
