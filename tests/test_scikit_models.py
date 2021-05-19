7e3ca7143c3b91e966163e6295b290c93958fcce
felipez <tifonzafel@gmail.com>
2021-05-19 16:22:53 +0200

    refactored the scikit interface tests #85

--- tests/test_scikit_models.py
+++ tests/test_scikit_models.py
@@ -6,39 +6,30 @@ from swan.modeller import SKModeller
 
 from .utils_test import PATH_TEST
 
+DATA = FingerprintsData(PATH_TEST / "thousand.csv", properties=["gammas"], sanitize=False)
+DATA.scale_labels()
 
-def test_decision_tree():
-    """Check the interface to the Decisiontree class."""
-    data = FingerprintsData(PATH_TEST / "thousand.csv", properties=["gammas"], sanitize=False)
 
-    data.scale_labels()
-    modeller = SKModeller(data, "decisiontree")
+def run_test(model: str, **kwargs):
+    """Run the training and validation step for the given model."""
+    modeller = SKModeller(DATA, model)
     modeller.train_model()
     predicted, expected = modeller.valid_model()
     reg = stats.linregress(predicted.flatten(), expected.flatten())
     assert reg.rvalue > 0
+
+
+def test_decision_tree():
+    """Check the interface to the Decisiontree class."""
+    run_test("decisiontree")
 
 
 def test_svm():
     """Check the interface to the support vector machine."""
-    data = FingerprintsData(PATH_TEST / "thousand.csv", properties=["gammas"], sanitize=False)
-
-    data.scale_labels()
-    modeller = SKModeller(data, "svm")
-    modeller.train_model()
-    predicted, expected = modeller.valid_model()
-    reg = stats.linregress(predicted.flatten(), expected.flatten())
-    assert reg.rvalue > 0
+    run_test("svm")
 
 
 def test_gaussian_process():
     """Check the interface to the support vector machine."""
-    data = FingerprintsData(PATH_TEST / "thousand.csv", properties=["gammas"], sanitize=False)
-
-    data.scale_labels()
     kernel = DotProduct() + WhiteKernel()
-    modeller = SKModeller(data, "gaussianprocess", kernel=kernel)
-    modeller.train_model()
-    predicted, expected = modeller.valid_model()
-    reg = stats.linregress(predicted.flatten(), expected.flatten())
-    assert reg.rvalue > 0
+    run_test("gaussianprocess", kernel=kernel)
