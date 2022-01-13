58d3a1b13e6346a2cef88f313366c1fccda65675
Aron <aronpjansen@gmail.com>
2022-01-13 13:35:13 +0100

    Added activation argument to fingerprint model

--- swan/modeller/models/fingerprint_models.py
+++ swan/modeller/models/fingerprint_models.py
@@ -8,14 +8,17 @@ __all__ = ["FingerprintFullyConnected"]
 class FingerprintFullyConnected(nn.Module):
     """Fully connected network for non-linear regression."""
 
-    def __init__(self, input_units: int = 2048, hidden_units: int = 100, output_units: int = 1):
+    def __init__(self, input_units: int = 2048, hidden_units: int = 100, output_units: int = 1,
+                activation: nn.Module = nn.ReLU):
         """Create a deep feed foward network."""
         super().__init__()
+        self.activation = activation
+
         self.net = nn.Sequential(
             nn.Linear(input_units, hidden_units),
-            nn.ReLU(),
+            self.activation(),
             nn.Linear(hidden_units, hidden_units),
-            nn.ReLU(),
+            self.activation(),
             nn.Linear(hidden_units, output_units),
         )
 
